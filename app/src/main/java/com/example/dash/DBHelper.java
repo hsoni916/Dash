1c5c9bda8a76c0c8e8719599e31e00367ebef5e4
Harsh <pataliaharsh948@gmail.com>
2022-04-15 05:05:42 +0530

    Commit 2.

--- app/src/main/java/com/example/dash/DBHelper.java
+++ app/src/main/java/com/example/dash/DBHelper.java
@@ -11,29 +11,36 @@ public class DBHelper extends SQLiteOpenHelper {
 
     static final String Username = "";
     static final String Password = "";
-    static final String DB_NAME = "Customers";
+    static final String DB_NAME = "MainFrame";
     static final int DB_VERSION = 1;
 
     public DBHelper(@Nullable Context context) {
         super(context, DB_NAME,null, DB_VERSION);
     }
 
-    private static final String CREATE_TABLE= "create table if not exists " + "Customers" +
+    private static final String CREATE_TABLE= "create table if not exists " + "CustomersList" +
             "("
             + "Name" +" TEXT NOT NULL,"
-            + "PhoneNumber" + "STRING NOT NULL,"
+            + "PhoneNumber" + "STRING PRIMARY KEY NOT NULL CHECK (length(PhoneNumber)==10),"
             + "DateofBirth" + "STRING NOT NULL,"
             + "Points" + "INTEGER"
+            + ");";
+
+    private static final String CREATE_TABLE_CONTROLS = "create table if not exists " + "Controls" +
+            "("
+            + "PointThreshold" + "INTEGER NOT NULL CHECK (PointThreshold>=100)"
             + ");";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_TABLE_CONTROLS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + "Customers");
+        db.execSQL("DROP TABLE IF EXISTS " + "CustomersList");
+        db.execSQL("DROP TABLE IF EXISTS " + "Controls");
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/dash/DBManager.java
+++ app/src/main/java/com/example/dash/DBManager.java
@@ -1,13 +1,15 @@
 package com.example.dash;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.util.Log;
 
 public class DBManager {
-
+    int GlobalPointTH = 0;
     private DBHelper dbHelper;
 
     private Context context;
@@ -30,15 +32,55 @@ public class DBManager {
     }
 
 
-    public void insert(String Name, String PhoneNumber, String DOB) {
+    public void insertNewCustomer(String Name, String PhoneNumber, String DOB) {
         Log.d("SQL","insert");
-        String sql = "INSERT INTO Customer (Name, PhoneNumber, DOB) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO Customers (Name, PhoneNumber, DOB) VALUES (?, ?, ?)";
         SQLiteStatement statement = database.compileStatement(sql);
         statement.bindString(0, Name);
         statement.bindString(1,PhoneNumber);
         statement.bindString(2, DOB);
         statement.executeInsert();
         close();
+    }
+
+    public void changePointThreshold(int newthreshold){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("PointThreshold",newthreshold);
+        db.insert("Controls", null, contentValues);
+        db.close();
+    }
+
+    public void addPoints(String Name, String PhoneNumber, int cashflow){
+        int initialPoints,pointsToAdd;
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String sql = "SELECT Points FROM Customers WHERE Name ='" + Name + "' AND PhoneNumber='" + PhoneNumber +"'";
+        String sql1 = "SELECT PointThreshold FROM Controls";
+        try  {
+            Cursor pointCursor = db.rawQuery(sql, null);
+            if(pointCursor.moveToFirst()){
+                if(pointCursor.getInt(0)!=0){
+                    Cursor pthCursor = db.rawQuery(sql1, null);
+                    GlobalPointTH = pthCursor.getInt(0);
+                    pointsToAdd = cashflow/GlobalPointTH;
+                    initialPoints = pointCursor.getInt(0);
+                    pointsToAdd = initialPoints+pointsToAdd;
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put("Points",pointsToAdd);
+                    db.insert("Customers",null,contentValues);
+                }else{
+                    //cashFlow divide by point decimal.
+                    //Add the result to
+                }
+            }
+        }catch (Exception e){
+            e.fillInStackTrace();
+        }
+
+    }
+
+    public void usePoints(String Name, String PhoneNumber, int points){
+
     }
 
 }
